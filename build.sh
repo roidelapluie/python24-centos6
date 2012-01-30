9fa12a75e86a579621afc593e7416ef6c43c6a56
Julien Pivotto <roidelapluie@inuits.eu>
2012-01-30 09:25:02 +0100

    fix typo

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@ wget -c http://www.python.org/ftp/python/${PYVERSION}/Python-${PYVERSION}.tar.bz
 wget -c http://www.python.jp/pub/JapaneseCodecs/JapaneseCodecs-${JPCODECS}.tar.gz -O SOURCES/JapaneseCodecs-${JPCODECS}.tar.gz
 cp -rv SOURCES/* ~/rpmbuild/SOURCES/
 sed -i 's/JENKINS_BUILD/'$BUILD_NUMBER'/' SPECS/python.spec
-[ -d tmp ] &7 rm -rvf tmp
+[ -d tmp ] && rm -rvf tmp
 mkdir -p tmp
 rpmbuild  --define="_topdir $PWD" --define="_tmppath $PWD/tmp" -ba SPECS/python.spec
