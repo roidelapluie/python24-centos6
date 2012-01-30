a5acf25519fbde15ba3ee13ccc32bee3cb06a2b8
Julien Pivotto <roidelapluie@inuits.eu>
2012-01-30 09:19:45 +0100

    add build script

---
+++ build.sh
@@ -0,0 +1,7 @@
+wget -c http://www.python.org/ftp/python/${PYVERSION}/Python-${PYVERSION}.tar.bz2 -O SOURCES/Python-${PYVERSION}.tar.bz2
+wget -c http://www.python.jp/pub/JapaneseCodecs/JapaneseCodecs-${JPCODECS}.tar.gz -O SOURCES/JapaneseCodecs-${JPCODECS}.tar.gz
+cp -rv SOURCES/* ~/rpmbuild/SOURCES/
+sed -i 's/JENKINS_BUILD/'$BUILD_NUMBER'/' SPECS/python.spec
+[ -d tmp ] &7 rm -rvf tmp
+mkdir -p tmp
+rpmbuild  --define="_topdir $PWD" --define="_tmppath $PWD/tmp" -ba SPECS/python.spec
